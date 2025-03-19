dca47a9f229bd30654924fdf9d2cc77433a0b39a
Shrini <shrinivasbtelalwar@gmail.com>
2025-03-19 12:33:40 +0530

    added some comments

--- task.sh
+++ task.sh
@@ -7,3 +7,6 @@ cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
 tail -100f /var/log/httpd/access_log
+
+
+#Added some comments as this is the sample file
