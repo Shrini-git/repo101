5b3b14eead5fb3a0f439fe7ea67cfe13cb171d7c
Shrini <shrinivasbtelalwar@gmail.com>
2025-03-19 16:30:46 +0530

    committed unmerged

--- task.sh
+++ task.sh
@@ -8,6 +8,7 @@ git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
 tail -100f /var/log/httpd/access_log
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> parent of c0a274b (task.sh)
@@ -19,3 +20,13 @@ this line is required for next try
 =======
 this is the line no. 13
 >>>>>>> eb439f3c826bdb341537626c90716c5337a68a9c
+=======
+systemctl stop httpd
+
+#Added some comments as this is the sample file
+=======
+systemctl restart httpd
+
+#Added some comments as this is the sample file (this comment is not required)
+>>>>>>> 1cbc2913d01d45e30cccbc4e5657cb1531d2b74b
+>>>>>>> parent of a03842c (restored changes in task.sh)
