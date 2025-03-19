a03842cfd3b8364eec1a98f5f3d2d76f51d21beb
Shrini <shrinivasbtelalwar@gmail.com>
2025-03-19 14:07:00 +0530

    restored changes in task.sh

--- task.sh
+++ task.sh
@@ -7,12 +7,3 @@ cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
 tail -100f /var/log/httpd/access_log
-<<<<<<< HEAD
-systemctl stop httpd
-
-#Added some comments as this is the sample file
-=======
-systemctl restart httpd
-
-#Added some comments as this is the sample file (this comment is not required)
->>>>>>> 1cbc2913d01d45e30cccbc4e5657cb1531d2b74b
