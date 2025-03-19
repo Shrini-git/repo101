cf4051a0dbc4cad55c1cfb72a75c081b7927bbff
Shrini <shrinivasbtelalwar@gmail.com>
2025-03-19 14:09:06 +0530

    made some changes on line no. 13

--- task.sh
+++ task.sh
@@ -7,3 +7,7 @@ cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
 tail -100f /var/log/httpd/access_log
+
+
+
+this line is required for next try
