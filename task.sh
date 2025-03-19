bb80c02137bebdc95a08328c36087d1b391b6701
Shrinivas Telalwar <shrinivasbtelalwar@gmail.com>
2025-03-19 14:11:05 +0530

    Update task.sh
    changes reverted

--- task.sh
+++ task.sh
@@ -7,11 +7,3 @@ cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* .
 tail -100f /var/log/httpd/access_log
-
-
-
-<<<<<<< HEAD
-this line is required for next try
-=======
-this is the line no. 13
->>>>>>> eb439f3c826bdb341537626c90716c5337a68a9c
